e7a2b04c252754b591bd6d3c8926428f06018091
laibanoor101 <164699732+laibanoor101@users.noreply.github.com>
2024-10-27 12:48:53 -0700

    inital commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.ts
+document.addEventListener('DOMContentLoaded', function () {
+    var headers = document.querySelectorAll('.section-header');
+    headers.forEach(function (header) {
+        header.addEventListener('click', function () {
+            var content = header.nextElementSibling;
+            if (content.style.display === 'none' || content.style.display === '') {
+                content.style.display = 'block';
+            }
+            else {
+                content.style.display = 'none';
+            }
+        });
+    });
+});
